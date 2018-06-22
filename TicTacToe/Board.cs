ac2508d5b4c2b5e6bbbb6e0f72f03a7ce4610a3c
Mike Choi <mike.choi@live.ca>
2018-06-22 16:12:11 -0700

    Added BoardPosition method to return board coordinate based on number input

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -14,6 +14,24 @@ namespace TicTacToe
         {
             state = new State[3, 3];
         }
+        
+        public Tuple<int,int> BoardPosition(int squareNum)
+        {
+            // Will follow keyboard numpad format
+            switch (squareNum)
+            {
+                case 2: return Tuple.Create(2, 1);
+                case 3: return Tuple.Create(2, 2);
+                case 4: return Tuple.Create(1, 0);
+                case 5: return Tuple.Create(1, 1);
+                case 6: return Tuple.Create(1, 2);
+                case 7: return Tuple.Create(0, 0);
+                case 8: return Tuple.Create(0, 1);
+                case 9: return Tuple.Create(0, 2);
+                default: return null;
+            }
+            
+        }
 
     }
 }
