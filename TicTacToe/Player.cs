371d2e092bd067afa749e6acc7ed747807416f51
Mike Choi <mike.choi@live.ca>
2018-06-22 16:10:47 -0700

    Added PlayerMove method to return board coordinate based on player input

--- TicTacToe/Player.cs
+++ TicTacToe/Player.cs
@@ -9,19 +9,31 @@ namespace TicTacToe
     class Player
     {
         public static Random rnd = new Random();
-        public string Name { get; }
+        public string name { get; }
         public int startNum { get; }
         private int squareNum;
 
         public Player(string name)
         {
-            Name = name;
-            this.startNum = rnd.Next(0, 101);
+            this.name = name;
+            startNum = rnd.Next(0, 101);
         }
         
-        public int PlayerMove (string squareNumString)
+        public Tuple<int,int> PlayerMove (Board board)
         {
-            squareNum = Convert.ToInt32(squareNumString);
+            do
+            {
+                Console.WriteLine("Enter board position: ");
+                string squareNumString = Console.ReadLine();
+                squareNum = Convert.ToInt32(squareNumString);
+                if (squareNum < 1 || squareNum > 9)
+                {
+                    Console.WriteLine("That is an incorrect input, try again.");
+                }
+            } while (squareNum < 1 || squareNum < 9);
+
+            var position = board.BoardPosition(squareNum);
+            return position;
         }
     }
 }
