ed96a3d329059952cd11bc861eff7d518289979b
Mike Choi <mike.choi@live.ca>
2018-06-22 18:49:35 -0700

    Renamed StartDecider to StartPlayer. Method also now decides who starts using Random class

---
+++ TicTacToe/StartPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class StartPlayer
+    {
+        private static Random rnd = new Random();
+        private int p1Num, p2Num;
+
+        public bool Order(string p1Name, string p2Name)
+        {
+            do
+            {
+                p1Num = rnd.Next(0, 101);
+                p2Num = rnd.Next(0, 101);
+            } while (p1Num == p2Num);
+            
+            Console.WriteLine("\nIt was randomly decided that: ");
+            Console.WriteLine((p1Num > p2Num) ? $"{p1Name} Starts!" : $"{p2Name} Starts!");
+
+            return (p1Num > p2Num) ? true : false; 
+        }
+    }
+}
