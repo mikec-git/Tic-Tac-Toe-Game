33eee41bedbc306fe9ac0e329e641c3a5c520048
Mike Choi <mike.choi@live.ca>
2018-06-23 00:27:34 -0700

    Added GameStarter method which outputs the 'X' player and 'O' player, based on the boolean output of the Order method

--- TicTacToe/StartPlayer.cs
+++ TicTacToe/StartPlayer.cs
@@ -11,18 +11,23 @@ namespace TicTacToe
         private static Random rnd = new Random();
         private int p1Num, p2Num;
 
-        public bool Order(string p1Name, string p2Name)
+        public void GameStarter(Player player1, Player player2, out Player xPlayer, out Player oPlayer)
+        {
+            xPlayer = (Order(player1.Name, player2.Name) ? player1 : player2);
+            oPlayer = (!Order(player1.Name, player2.Name) ? player1 : player2);
+
+            Console.WriteLine($"\nIt was randomly decided that {xPlayer.Name} starts!");
+        }
+
+        private bool Order(string p1Name, string p2Name)
         {
             do
             {
                 p1Num = rnd.Next(0, 101);
                 p2Num = rnd.Next(0, 101);
             } while (p1Num == p2Num);
-            
-            Console.WriteLine("\nIt was randomly decided that: ");
-            Console.WriteLine((p1Num > p2Num) ? $"{p1Name} Starts!" : $"{p2Name} Starts!");
-
             return (p1Num > p2Num) ? true : false; 
         }
+
     }
 }
