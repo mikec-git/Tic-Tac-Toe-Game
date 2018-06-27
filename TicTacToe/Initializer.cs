75cc81414fc3eb4453fcc0da25958dfc9cfd4e2b
Mike Choi <mike.choi@live.ca>
2018-06-26 20:16:03 -0700

    Initializer class prints out game description, takes player names, and prints winner

---
+++ TicTacToe/Initializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    static class Initializer
+    {
+        static public void Intro(out string p1Name, out string p2Name)
+        {
+            Console.Title = "Mike's Tic Tac Toe Game";
+            Console.WriteLine("Mike's Tic Tac Toe Game!");
+            Console.WriteLine("Press any key to continue: ");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.Write("Type the name of Player 1: ");
+            p1Name = Console.ReadLine();
+            Console.Write("Type the name of Player 2: ");
+            p2Name = Console.ReadLine();
+        }
+
+        static public void Ending(States winner, string xName, string oName)
+        {
+            if (winner == States.X)
+            {
+                Console.Write("\nThe winner of this round is: ");
+                Console.WriteLine($"{xName}");
+            }
+            else if (winner == States.O)
+            {
+                Console.Write("\nThe winner of this round is: ");
+                Console.WriteLine($"{oName}");
+            }
+        }
+    }
+}
